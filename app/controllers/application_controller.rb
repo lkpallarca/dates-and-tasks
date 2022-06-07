02ccb1d92e0c9217b1f976c42c6af7d09723f10b
lkpallarca <Ckencepallarca@gmail.com>
2022-06-07 10:39:43 +0800

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def after_sign_in_path_for(resource_or_scope)
+    calendar_path
+  end
+  
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password)}
+  end
+end
